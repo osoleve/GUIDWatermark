c41dce86482e3d47081cf9e082fd848d7dfb7f8a
Oso <andrew.levenson@gmail.com>
2021-08-12 13:10:24 -0400

    Merge branch 'dev'
    # Conflicts:
    #	src/Watermark.hs

--- src/Watermark.hs
+++ src/Watermark.hs
@@ -18,6 +18,9 @@ import qualified Data.List                     as L
 import           Data.Maybe
 import           Data.Ord
 
+import Watermark.Utils.Conversion
+import Watermark.Utils.GUID
+
 type GUID = String
 type ClientID = GUID
 type WatermarkedGUID = GUID
@@ -37,24 +40,6 @@ watermark clientid (preprocess -> guid) = reformat $ zipWith processChar bitmask
     patternNum  = fromIntegral $ hexToInteger guid `mod` toInteger numPatterns
     bitmask     = take guidLength . drop (guidLength * patternNum) $ fingerprint
 
-
--- | Remove the hyphens from a GUID
-preprocess :: GUID -> GUID
-preprocess = filter (/= '-')
-
--- | Add the hyphens back into a GUID
-reformat :: GUID -> GUID
-reformat guid = L.intercalate "-" spans
-  where
-    spans = map
-        ($ guid)
-        [ take 8
-        , take 4 . drop 8
-        , take 4 . drop 12
-        , take 4 . drop 16
-        , take 12 . drop 20
-        ]
-
 clientIDToFingerprint :: ClientID -> Fingerprint
 clientIDToFingerprint = toFullLength . concatMap show . tail . toBin . hexToInteger
   where
@@ -62,37 +47,6 @@ clientIDToFingerprint = toFullLength . concatMap show . tail . toBin . hexToInte
     toFullLength :: String -> String
     toFullLength b | length b <= fullLength = leftPad (fullLength - length b) b
                    | otherwise              = take fullLength b
-
-leftPad :: Int -> String -> String
-leftPad 0 s = s
-leftPad n s = replicate n '0' ++ s
-
-toBin :: Integer -> [Int]
-toBin 0 = [0]
-toBin n = flip (++) [n' `mod` 2] $ toBin (n `div` 2) where n' = fromIntegral n
-
-hexToInteger :: String -> Integer
-hexToInteger [] = 0
-hexToInteger h  = hexCharToInt (last h) + (16 * hexToInteger (init h))
-  where
-    hexCharToInt :: Char -> Integer
-    hexCharToInt (toLower -> c) =
-        toInteger . fromJust $ c `L.elemIndex` "0123456789abcdef"
-
-integerToGUID' :: Integer -> GUID
-integerToGUID' 0 = []
-integerToGUID' n = intToHexChar (n `mod` 16) : integerToGUID' (n `div` 16)
-  where
-    intToHexChar :: Integer -> Char
-    intToHexChar x | x < 10    = head $ show x
-                   | otherwise = chr (fromIntegral $ 65 + (x - 10))
-
-integerToGUID :: Integer -> GUID
-integerToGUID 0 = replicate 32 '0'
-integerToGUID x = leftPad n x'
-  where
-    x' = integerToGUID' x
-    n = 32 - length x'
 
 fingerprintToClientID :: Fingerprint -> ClientID
 fingerprintToClientID = reformat . integerToGUID . toDec
@@ -111,7 +65,6 @@ recoverFingerprint (map preprocess -> guids) = concat pieces
     groups        = L.groupBy (\x y -> getGroup x == getGroup y) guids'
     partialGroups = (map . map) recoverPartial groups
     pieces        = map mergePartials partialGroups
-
 
 recoverPartial :: WatermarkedGUID -> PartialFingerprint
 recoverPartial [] = []
---
+++ src/Watermark/Utils/GUID.hs
@@ -0,0 +1,17 @@
+module Watermark.Utils.GUID
+    (preprocess, reformat) where
+
+import qualified Data.List                     as L
+
+type GUID = String
+-- | Remove the hyphens from a GUID
+preprocess :: GUID -> GUID
+preprocess = filter (/= '-')
+
+-- | Add the hyphens back into a GUID
+reformat :: GUID -> GUID
+reformat guid = L.intercalate "-" spans
+  where
+    spans = map
+        ($ guid)
+        [take 8, take 4 . drop 8, take 4 . drop 12, take 4 . drop 16, take 12 . drop 20]
