be52722c85936380e3abc45a207540ec5c2f34e0
Oso <andrew.levenson@gmail.com>
2021-08-11 22:14:57 -0400

    Updated algo: Now uses 4 fingerprints per client, clientid can be up to a 128-bit integer.

--- src/Watermark.hs
+++ src/Watermark.hs
@@ -7,25 +7,36 @@ module Watermark
     , clientIDToFingerprint
     , fingerprintToClientID
     , preprocess
+    , reformat
+    , hexToInteger
+    , toBin
+    , integerToGUID
     ) where
 
 import           Data.Char
 import qualified Data.List                     as L
+import           Data.Maybe
+import           Data.Ord
 
-type ClientID = Int -- ^ Int in range [0, 2^32-1]
 type GUID = String
+type ClientID = Integer -- ^ Int in range [0, 2^32-1]
 type WatermarkedGUID = String
 type Fingerprint = String
 type PartialFingerprint = String
 
 guidLength = 32
+numPatterns = 4
 
 watermark :: ClientID -> GUID -> GUID
-watermark clientid (preprocess -> guid) =
-    let processChar :: Char -> Char -> Char
-        processChar b c | b == '0' || isDigit c = c
-                        | otherwise             = toLower c
-    in  reformat $ zipWith processChar (clientIDToFingerprint clientid) guid
+watermark clientid (preprocess -> guid) = reformat $ zipWith processChar bitmask guid
+  where
+    processChar :: Char -> Char -> Char
+    processChar b c | b == '0' || isDigit c = c
+                    | otherwise             = toLower c
+    fingerprint = clientIDToFingerprint clientid
+    patternNum  = fromIntegral $ hexToInteger guid `mod` toInteger numPatterns
+    bitmask     = take guidLength . drop (guidLength * patternNum) $ fingerprint
+
 
 -- | Remove the hyphens from a GUID
 preprocess :: GUID -> GUID
@@ -46,29 +57,62 @@ reformat guid = L.intercalate "-" spans
         ]
 
 clientIDToFingerprint :: ClientID -> Fingerprint
-clientIDToFingerprint = to32Bits . concatMap show . tail . toBin
+clientIDToFingerprint = toFullLength . concatMap show . tail . toBin
   where
-    toBin :: Int -> [Int]
-    toBin 0 = [0]
-    toBin n = toBin (div n 2) ++ [mod n 2]
-    to32Bits :: String -> String
-    to32Bits b | length b <= guidLength = leftPad (guidLength - length b) b
-               | otherwise              = take guidLength b
-    leftPad :: Int -> String -> String
-    leftPad 0 s = s
-    leftPad n s = replicate n '0' ++ s
+    fullLength = guidLength * fromIntegral numPatterns
+    toFullLength :: String -> String
+    toFullLength b | length b <= fullLength = leftPad (fullLength - length b) b
+                   | otherwise              = take fullLength b
+
+leftPad :: Int -> String -> String
+leftPad 0 s = s
+leftPad n s = replicate n '0' ++ s
+
+toBin :: Integer -> [Int]
+toBin 0 = [0]
+toBin n = flip (++) [n' `mod` 2] $ toBin (n `div` 2) where n' = fromIntegral n
+
+hexToInteger :: String -> Integer
+hexToInteger [] = 0
+hexToInteger h  = hexCharToInt (last h) + (16 * hexToInteger (init h))
+  where
+    hexCharToInt :: Char -> Integer
+    hexCharToInt (toLower -> c) =
+        toInteger . fromJust $ c `L.elemIndex` "0123456789abcdef"
+
+integerToGUID' :: Integer -> GUID
+integerToGUID' 0 = []
+integerToGUID' n = intToHexChar (n `mod` 16) : integerToGUID' (n `div` 16)
+  where
+    intToHexChar :: Integer -> Char
+    intToHexChar x | x < 10    = head $ show x
+                   | otherwise = chr (fromIntegral $ 65 + (x - 10))
+
+integerToGUID :: Integer -> GUID
+integerToGUID 0 = replicate 32 '0'
+integerToGUID x = leftPad n x'
+  where
+    x' = integerToGUID' x
+    n = 32 - length x'
 
 fingerprintToClientID :: Fingerprint -> ClientID
 fingerprintToClientID = toDec
   where
-    toDec :: String -> Int
-    toDec = L.foldl' (\acc x -> acc * 2 + digitToInt x) 0
+    toDec :: String -> Integer
+    toDec = L.foldl' (\acc x -> acc * 2 + (toInteger . digitToInt) x) 0
 
 recoverClientID :: [WatermarkedGUID] -> ClientID
 recoverClientID = fingerprintToClientID . recoverFingerprint
 
 recoverFingerprint :: [WatermarkedGUID] -> Fingerprint
-recoverFingerprint = mergePartials . map (recoverPartial . preprocess)
+recoverFingerprint (map preprocess -> guids) = concat pieces
+  where
+    getGroup      = (`mod` numPatterns) . hexToInteger
+    guids'        = L.sortOn getGroup guids
+    groups        = L.groupBy (\x y -> getGroup x == getGroup y) guids'
+    partialGroups = (map . map) recoverPartial groups
+    pieces        = map mergePartials partialGroups
+
 
 recoverPartial :: WatermarkedGUID -> PartialFingerprint
 recoverPartial [] = []
--- test/test.hs
+++ test/test.hs
@@ -1,4 +1,4 @@
 import           Watermark
 
--- Sample GUIDs watermarked with ClientID 1671437891
+-- Sample GUIDs watermarked with ClientID 65484653464635436017066
 main = recoverClientID . lines <$> readFile "test/watermarked_guids.txt"
